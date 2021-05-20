15817507a7ca70e2ac6caa705dc2fbc0cfb37efb
ilya5006 <ilya.samarczew2011@yandex.ru>
2021-05-20 16:33:12 +0300

    Admin page layout

---
+++ views/admin.php
@@ -0,0 +1,8 @@
+<h1 class="page-title">Панель администратора</h1>
+
+<div class="admin-panel">
+    <a class="admin-panel-button" href="">Создать учётную запись студента</a>
+    <a class="admin-panel-button" href="">Редактировать наборы слов</a>
+    <a class="admin-panel-button" href="">Создать набор слов</a>
+    <a class="admin-panel-button" href="">Все учётные записи</a>
+</div>
