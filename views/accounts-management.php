26a93be5071d8ab4dbb728aca5c67bad274960f4
ilya5006 <ilya.samarczew2011@yandex.ru>
2021-05-25 19:14:38 +0300

    Account management page layout

---
+++ views/accounts-management.php
@@ -0,0 +1,18 @@
+<h1 class="page-title">Панель администратора</h1>
+
+
+<div class="acсounts-management-panel">
+    <h2 class="admin-panel-page-title">Наборы слов</h2>
+    <input class="search" type="text" placeholder="Поиск">
+
+    <button class="account enumeration">
+        <span class="name">Аккаунт 1</span>
+        <span>|</span>
+        <span>3719</span>
+    </button>
+    <button class="account enumeration">Набор слов 2</button>
+    <button class="account enumeration">Набор слов 3</button>
+    <button class="account enumeration">Набор слов 4</button>
+    <button class="account enumeration">Набор слов 5</button>
+    
+</div>
