4b1b71435f6faa254508e7ef3d6d82b49d6a69dc
ilya5006 <ilya.samarczew2011@yandex.ru>
2021-05-25 19:18:47 +0300

    Wordsset create page layout

---
+++ views/wordsset-create.php
@@ -0,0 +1,11 @@
+<h1 class="page-title">Панель администратора</h1>
+
+<form class="student-registration" method="POST" enctype="multipart/form-data" action="./../model/wordsset-create.php">
+    <h2 class="admin-panel-page-title">Создать набор слов</h2>
+    
+    <input type="text" name="name" placeholder="Название">
+    <label for="file">Выберите файл</label>
+    <input id="file" style="display: none;" type="file" name="file">
+    <input type="submit" value="Создать">
+
+</form>
