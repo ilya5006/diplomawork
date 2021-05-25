fb8af3debe14ec3a13ea028ad187ddee13748063
ilya5006 <ilya.samarczew2011@yandex.ru>
2021-05-25 19:22:16 +0300

    Wordsset-editor page layout

---
+++ views/wordsset-editor.php
@@ -0,0 +1,40 @@
+<h1 class="page-title">Панель администратора</h1>
+
+<div class="words">
+    <h2 class="admin-panel-page-title">
+        Редактирование набора слов 
+        <br>
+        "Для программистов"
+    </h2>
+
+
+    <div class="words-container">
+        <p class="word">Слово</p>
+        <p class="word">Слово</p>
+        <p class="word">Слово</p>
+        <p class="word">Слово</p>
+        <p class="word">Словоdwadwad</p>
+        <p class="word">Слово</p>
+        <p class="word">Слово</p>
+        <p class="word">Слово</p>
+        <p class="word">Слово</p>
+        <p class="word">СловWADWAdwadwaо</p>
+        <p class="word">Слово</p>
+        <p class="word">Слово</p>
+        <p class="word">СловDawdadо</p>
+        <p class="word">Слово</p>
+        <p class="word">Слово</p>
+        <p class="word">Слово</p>
+        <p class="word">Слово</p>
+        <p class="word">Слово</p>
+        <p class="word">Слово</p>
+        <p class="word">Слово</p>
+        <p class="word">Слово</p>
+    </div>
+    
+    <input type="text" class="enter-new-word" placeholder="Введите слово">
+    <button class="add-new-word">Добавить</button>
+</div>
+
+<button class="update">Обновить</button>
+<button class="delete">Удалить выбранное</button>
