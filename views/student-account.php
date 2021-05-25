ad22c7f88c0d971f228fccd9aa2a23a4bf7753a4
ilya5006 <ilya.samarczew2011@yandex.ru>
2021-05-25 19:15:36 +0300

    Student account page layout

---
+++ views/student-account.php
@@ -0,0 +1,13 @@
+<h1 class="page-title">Панель администратора</h1>
+
+<div class="wrapper">
+    <h2 class="admin-panel-page-title">Учётная запись студента</h2>
+    
+    <div class="student-account">
+        <p class="fio">USER_FIO</p>
+        <p class="login">USER_LOGIN</p>
+        <p class="group">3719</p>
+        <p class="role">Студент</p>
+        <a href="">Удалить аккаунт</a>
+    </div>
+</div>
