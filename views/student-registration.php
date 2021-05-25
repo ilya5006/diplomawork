f6a13cf5a9aa82440adac41d6268d9f49388438e
ilya5006 <ilya.samarczew2011@yandex.ru>
2021-05-25 19:17:52 +0300

    Student registration page layout

---
+++ views/student-registration.php
@@ -0,0 +1,11 @@
+<h1 class="page-title">Панель администратора</h1>
+
+<form class="student-registration" method="POST" action="./../model/student-registration.php">
+    <h2 class="admin-panel-page-title">Регистрация студента</h2>
+    
+    <input type="text" name="login" placeholder="Логин">
+    <input type="text" name="Пароль" placeholder="Пароль">
+    <input type="text" name="fio" placeholder="ФИО">
+    <input type="text" name="group" placeholder="Группа">
+    <input type="submit" value="Регистрация">
+</form>
