4b61f132f36072aadec7acccac78cc35c3d28df7
ilya5006 <ilya.samarczew2011@yandex.ru>
2021-05-25 19:12:49 +0300

    Admin page

--- views/admin.php
+++ views/admin.php
@@ -1,8 +1,8 @@
 <h1 class="page-title">Панель администратора</h1>
 
 <div class="admin-panel">
-    <a class="admin-panel-button" href="">Создать учётную запись студента</a>
-    <a class="admin-panel-button" href="">Редактировать наборы слов</a>
-    <a class="admin-panel-button" href="">Создать набор слов</a>
-    <a class="admin-panel-button" href="">Все учётные записи</a>
+    <a class="admin-panel-button" href="/student-registration.php">Создать учётную запись студента</a>
+    <a class="admin-panel-button" href="/wordssets.php">Редактировать наборы слов</a>
+    <a class="admin-panel-button" href="/wordsset-create.php">Создать набор слов</a>
+    <a class="admin-panel-button" href="/accounts-management.php">Все учётные записи</a>
 </div>
