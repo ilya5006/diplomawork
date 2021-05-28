f6340c290c2b41d00881d4ecea5907e67fac2663
ilya5006 <ilya.samarczew2011@yandex.ru>
2021-05-28 18:21:04 +0300

    signin => login

--- login.php
+++ login.php
@@ -9,13 +9,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Spectral+SC:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="./assets/css/main.css">
-    <link rel="stylesheet" href="./assets/css/signin.css">
+    <link rel="stylesheet" href="./assets/css/login.css">
 
     <title>Авторизация</title>
 </head>
 <body>
     <?php 
-    include './views/signin.php';
+    include './views/login.php';
     ?>
 </body>
 </html>
